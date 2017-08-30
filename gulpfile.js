9f658c90aca3c19d943283da54bdab5b9eaef72a
nyancodeid <ryandevstudio@gmail.com>
2017-08-30 15:56:34 +0700

    update js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp');
 var ngAnnotate = require('gulp-ng-annotate');
-var obfuscate = require('gulp-obfuscate');
+var obfuscate = require('gulp-javascript-obfuscator');
+var uglify = require('gulp-uglify');
+var bytediff = require('gulp-bytediff');
+var plumber = require('gulp-plumber');
  
 gulp.task('default', function () {
     return gulp.src('./source/app.module.js')
-    	.pipe(ngAnnotate())
-        .pipe(obfuscate())
+    	.pipe(plumber())
+			.pipe(bytediff.start())
+    			.pipe(ngAnnotate())
+				.pipe(obfuscate())
+				.pipe(uglify({mangle: true}))
+			.pipe(bytediff.stop())
+		.pipe(plumber.stop())
         .pipe(gulp.dest('./static/js'));
 });
